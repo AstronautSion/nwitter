35781c5f94c831e63a10880afea3056059689406
AstronautSion <astronaut.sion@gmail.com>
2021-02-10 05:34:55 +0900

    #2.0 Using Firebase Auth

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import firebase from './firebase';
 
-console.log(firebase)
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />,document.getElementById('root'));
  
